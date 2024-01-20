44c87e6d121b49f7f8a8b1957151cede102d2591
Uzair Ahmad <59066113+Uzair-boot@users.noreply.github.com>
2024-01-21 00:48:48 +0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -25,4 +25,30 @@ router.get('/', async (req, res) => {
         res.status(500).json(err)
     }
 })
+
+// const updateProducts = async () => {
+//   try {
+//     const products = await Product.find();
+
+//     for (const [index, product] of products.entries()) {
+//       if ((index + 1) % 15 === 0) {
+//         product.category = "Gucci";
+//       } else if ((index + 1) % 3 === 0) {
+//         product.category = "Adidas";
+//       } else if ((index + 1) % 5 === 0) {
+//         product.category = "Nike";
+//       } else {
+//         product.category = "Bata";
+//       }
+
+//       await product.save();
+
+//       console.log(product);
+//     }
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+// updateProducts();
 module.exports = router;
